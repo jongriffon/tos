2477f846e412fc369cb0eef6fe4c8935ea0f3ee9
theovesy <theovesy@hotmail.fr>
2022-02-19 15:09:33 +0100

    save

---
+++ script.js
@@ -0,0 +1,28 @@
+function tos_lang(lang) {
+	var tos = document.getElementById("tos");
+	if (lang == 'fr')
+	{
+		tos.src = "tosfr.html";
+		tos.height = 3300;
+	}
+	else if (lang == 'en')
+	{
+		tos.src = "tosen.html";
+		tos.height = 2800;
+	}
+}
+
+function pricelist() {
+	var tos = document.getElementById("tosdiv");
+	var pl = document.getElementById("pricelist");
+	pl.hidden = !pl.hidden;
+	tos.hidden = !tos.hidden;
+	if (tos.hidden)
+	{
+		document.getElementById("plbtn").innerHTML = "TOS";
+	}
+	else
+	{
+	document.getElementById("plbtn").innerHTML = "Pricelist";
+	}
+}
